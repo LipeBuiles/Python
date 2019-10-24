46f68ee1a525c454bdd46763429b933fa0d49cdd
Juan Felipe Builes <44440920+elpinchepastel@users.noreply.github.com>
2019-10-24 01:00:19 -0500

    datatype and debug python 3.7

---
+++ datatype.py
@@ -0,0 +1,55 @@
+#String
+print("Hello The Pinche Pastel")
+print('Hello The Pinche Pastel')
+print('''Hello The Pinche Pastel''')
+print("""Hello The Pinche Pastel""")
+print("Hello"+" "+"The Pinche Pastel") #Concatenate
+
+#Numbers
+print(31) #Integer
+print(31.7) #Float
+
+#Boolean
+print(True)
+print(False)
+
+#List
+[10, 20, 30, 50]
+["Hello", "The", "Pinche", "Pastel"]
+[10, "Pastel", 31.5, False]
+[] #Empty
+
+#Tuplas
+(1, 2, 3, 4)
+() #Empty
+
+#Dictorionies
+{
+    "Article":"The",
+    "Name":"Pinche",
+    "Last Name":"Pastel",
+}
+{
+    "Age":31,
+    "name":"Pinche",
+    "Height":1.82,
+}
+{} #Empty
+
+#None
+None
+
+#Type
+print(type("Hello The Pinche Pastel"))
+print(type(31))
+print(type(31.5))
+print(type(False))
+print(type([10, "Pastel", 31.5, False]))
+print(type((1, 2, 3, 4)))
+print(type(
+{
+    "Article":"The",
+    "name":"Pinche",
+    "last_name":"Pastel",
+}))
+print(type(None))
