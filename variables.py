7f0c5df90fba036761fb427953cb336986c54164
Juan Felipe Builes <44440920+elpinchepastel@users.noreply.github.com>
2019-10-25 18:43:12 -0500

    variables

---
+++ variables.py
@@ -0,0 +1,32 @@
+#Case sensitive
+name = "The Pinche Pastel"
+Name = "Pastel"
+_name = "Pinche"
+print(name)
+print(Name)
+print(_name)
+
+x, book = 31, "No limit"
+print(x, book)
+
+book_name = "100 años de soledad"  # Snake Case
+bookName = "Cronica de una muere anunciada"  # Camel Case
+BookName = "Los funerales de la mamá grande"  # Pascal Case
+
+# Constants - In uppercase
+PI = 3.1416
+MY_NAME = "The Pinche Pastel"
+
+#Dynamic typing
+z = "Pastel"
+z = 31
+z = [10, 20, 30, 50]
+z = (1, 2, 3, 4)
+z = {
+    
+    "Article": "The",
+    "Name": "Pinche",
+    "Last Name": "Pastel",
+}
+z = True
+print(z)
