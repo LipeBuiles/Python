d27fd73102ccf7baeebdf95bb4055bdf3a7f715d
Juan Felipe Builes <44440920+elpinchepastel@users.noreply.github.com>
2019-11-05 03:17:14 -0500

    list

---
+++ lists.py
@@ -0,0 +1,44 @@
+test=[1, "Pastel", ["The", 33.33]]
+colors=["blue", "red", "green"]
+
+numbers_list=list((1,2,3,4))
+print((numbers_list))
+print(type(numbers_list))
+
+#Range
+from timeit import default_timer as timer
+start = timer()
+
+count=list(range(1,6))
+print(count)
+
+end = timer()
+time_out=end-start
+print("Time in seconds is {0}".format(time_out)) 
+
+#methods dir
+print(dir(colors))
+print(len(test))
+print(test[2])  
+print("blue" in colors)
+print(colors)
+colors[0]="black"
+print(colors)
+colors.append("white")
+print(colors)
+colors.extend(["brown", "violet"])
+print(colors)
+colors.insert(1, "pink")
+print(colors)
+colors.pop()
+print(colors)
+colors.pop(0)
+print(colors)
+colors.remove("red")
+print(colors)
+colors.sort()
+#colors.clear()
+print(colors)
+colors.sort(reverse=True)
+print(colors)
+print(colors.index("green"))
