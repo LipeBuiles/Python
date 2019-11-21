b4f616dc37f4f3325c7afd1151e7375a87eca436
Juan Felipe Builes <44440920+elpinchepastel@users.noreply.github.com>
2019-11-21 01:33:59 -0500

    dictionaries

---
+++ Learn/dictionaries.py
@@ -0,0 +1,28 @@
+produts = {
+    "name": "book",
+    "price": 15000,
+    "cuantity": 2
+}
+
+person = {
+    "firt_name": "Juan Felipe",
+    "last_name": "Builes"
+}
+
+print(person)
+print(type(person))
+
+#Methods with dir
+print(produts.keys())
+print(produts.items())
+
+produts.clear()
+print(produts)
+#del produts
+print(produts)
+
+products = [
+    {"name": "book", "price": 25000},
+    {"name": "pc", "price": 1750000},
+    {"name": "table", "price": 800000}
+]
