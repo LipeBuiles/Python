35680d7e6a7e9fce871bf098dc5c82339dbbb998
Juan Felipe Builes <44440920+elpinchepastel@users.noreply.github.com>
2019-11-21 01:15:52 -0500

    set

---
+++ Learn/set.py
@@ -0,0 +1,12 @@
+colors = {"yellow", "blue", "red"}
+print(colors)
+print(type(colors))
+print("red" in colors)
+
+#Methods with dir
+colors.add("green")
+print(colors)
+colors.remove("blue")
+print(colors)
+colors.clear()
+print(colors)
