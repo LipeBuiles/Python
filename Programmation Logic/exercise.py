18c5319c8410c43c6d2bf7f636b2aa4787c67ce3
Juan Felipe Builes <44440920+elpinchepastel@users.noreply.github.com>
2020-02-03 14:20:39 -0500

    exercise two - bigger number

--- Programmation Logic/exercise.py
+++ Programmation Logic/exercise.py
@@ -1,4 +1,4 @@
-#IS NUMBER PAR OR NOT?
+#1. Build a program that asks for a number and then says if this number is even or not?
 print("Ingrese un número:")
 num=int(input())
 
@@ -7,3 +7,12 @@ if module.modulo(num) == 0:
     print("El número {0} es par".format(num))
 else:
     print("El número {0} es impar".format(num))
+
+#2. Build a program that asks for 3 numbers on the screen and then say which is the largest of the numbers entered?
+print("Ingrese tres numeros:")
+num1=int(input())
+num2=int(input())
+num3=int(input())
+
+from module import bigger_number
+print("El mayor de los tres números es {0}".format(bigger_number(num1,num2,num3)))
--- Programmation Logic/module.py
+++ Programmation Logic/module.py
@@ -1,2 +1,14 @@
 def modulo(num):
     return num%2
+
+def bigger_number(num1, num2, num3):
+    if num1>=num2 and num1>=num3:
+        n=num1
+        return n
+    else:
+        if num2>=num1 and num2>=num3:
+            n=num2
+            return n
+        else:
+            n=num3
+            return n
