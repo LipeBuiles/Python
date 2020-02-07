c2c2d3a112fae0c87990ea31241c05697e3278e1
Juan Felipe Builes <44440920+elpinchepastel@users.noreply.github.com>
2020-02-07 18:06:03 -0500

    exercise four - multiple of a number

--- Programmation Logic/exercise.py
+++ Programmation Logic/exercise.py
@@ -18,10 +18,22 @@ from module import bigger_number
 print("El mayor de los tres números es {0}".format(bigger_number(num1,num2,num3)))
 
 #3. Build a program that asks for 3 numbers on the screen and then says phones is the largest, the middle one and the smallest of the entered numbers
+print("Ingrese tres numeros:")
 num1=int(input())
 num2=int(input())
 num3=int(input())
 
-from module import orden
+from module import order
+print("{0}".format(order(num1,num2,num3)))
 
-print("Los números organizados son: {0}".format(orden(num1,num2,num3)))
+#4. Build a program that asks for two numbers and says if the second is a multiple of the first.
+print("Ingrese el primer número: ")
+num1=int(input())
+print("Ingrese el segundo número: ")
+num2=int(input())
+
+from module import multiple
+if multiple(num1,num2) == 0:
+    print("El número: {1}, es múltiplo de: {0}".format(num1,num2))
+else:
+    print("El número: {1}, no es múltiplo de: {0}".format(num1,num2))
--- Programmation Logic/module.py
+++ Programmation Logic/module.py
@@ -1,7 +1,7 @@
 def modulo(num):
     return num%2
 
-def bigger_number(num1, num2, num3):
+def bigger_number(num1,num2,num3):
     if num1>=num2 and num1>=num3:
         n=num1
         return n
@@ -13,9 +13,12 @@ def bigger_number(num1, num2, num3):
             n=num3
             return n
         
-def orden (num1, num2, num3):
-    a = min(num1, num2, num3)
-    b = max(num1, num2, num3)
-    c = (num1+num2+num3)-a-b
-    e = [a,b,c]
+def order (num1,num2,num3):
+    a = min(num1,num2,num3)
+    c = max(num1,num2,num3)
+    b = (num1+num2+num3)-a-c
+    e = "El número mayor es: {0}\nEl número del medio es: {1}\nEl número menor es: {2}".format(c,b,a)
     return e
+
+def multiple(num1,num2):
+    return num1%num2
