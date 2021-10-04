91c3ef5db39b168c62e2ca520d045b142e7e8e93
Juan Felipe Builes <44440920+elpinchepastel@users.noreply.github.com>
2021-10-04 11:22:10 -0500

    migrations sqlLite

--- Curso django/mysite/mipagina/models.py
+++ Curso django/mysite/mipagina/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Personas(models.Model):
+    nombre = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=50, verbose_name="La direccion")
+    email = models.EmailField(blank=True, null=True)
+    telefono = models.CharField(max_length=11)
+
