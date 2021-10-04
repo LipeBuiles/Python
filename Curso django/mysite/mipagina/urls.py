079db13fa4cb1e408b0c03b783d4667c81a0a54a
Juan Felipe Builes <44440920+elpinchepastel@users.noreply.github.com>
2021-10-04 10:07:41 -0500

    rest url index

--- Curso django/mysite/mipagina/urls.py
+++ Curso django/mysite/mipagina/urls.py
@@ -4,7 +4,7 @@ from django.views.generic.base import View
 from . import views
 
 urlpatterns = [
-    path('inicio', views.index, name='index'),
+    path('', views.index, name='index'),
     path('hora/', views.hora, name='hora'),
     path('temas/', views.bienvenida, name='bienvenida'),
     path('barras/', views.barras, name='barras'),
